661d41a7e5c8545140cc9f95c7e34b85d9b4d815
Raj kashyap <rajkashyap.ki05@gmail.com>
2026-02-02 13:28:19 +0530

    Update In next lacture

--- Express_Backend/index.js
+++ Express_Backend/index.js
@@ -4,15 +4,50 @@ const app = express();
 
 const PORT =8000;
 
-app.get("/users",(req, res)=>{
+const Student=[
+    {id:2,name:"raj", branch: "cse"},
+    {id:4,name:"Kishan", branch: "ec"},
+    {id:3,name:"Kashyap", branch: "dep"},
+    {id:7,name:"Rohit", branch: "cld"},
+    {id:8,name:"kunal", branch: "dep"}
+
+
+];
+
+
+app.get("/",(req, res)=>{
     res.send("<h1>wellcome to home page</h1>")
 })
 
-app.get("/users/:id",(req, res)=>{
-    const userId= req.params.id
-    res.send("you are requesting for user:${userID}")
+app.get("/Student",(req, res)=>{
+   res.json(Student);
 })
 
 app.listen(PORT, ()=>{
     console.log('server is running on port:${PORT}')
 })
+
+// app.get("/Student/:id", (req, res)=>{
+//     const id = req.params.id;
+//     const arrayindex = Student.findIndex(s=>s.id==id);
+//     const data =Student[arrayindex];
+//     res.json(data);
+// })
+
+// Search through id
+app.get("/Student/:id", (req, res)=>{
+    const id = req.params.id;
+    const arrayindex = Student.findIndex(s=>s.id==id);
+    const data =Student[arrayindex];
+    res.json(data);
+})
+
+// Search through branch
+
+app.get("/Student/:branch/branch", (req, res)=>{
+    const branch = req.params.branch;
+    console.log(branch)
+    const data = Student.filter(s=>s.branch==branch);
+    res.json(data);
+})
+
